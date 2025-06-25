d9ec9edef383b53dc243d21e4354e04a8da93248
boxbox <boxbox210077@gmail.com>
2025-06-25 17:00:54 +0800

    debug_1

--- project/test.py
+++ project/test.py
@@ -1,8 +1,17 @@
-import mysql.connector
-db = mysql.connector.connect(
-    host = "localhost",
-    user = "DBuser",
-    password = "1234",
-    database = "world"
-)
-print("連線成功")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+url = 'https://hk.finance.yahoo.com/quote/9992.HK/history/?period1=1609459200&period2=1750838777'
+response = requests.get(url)
+if response.status_code == 200:
+    html_content = response.text
+else:
+    print(f"Failed to retrieve the page.Status code:{response.status_code}")
+    exit()
+soup = BeautifulSoup(html_content,'lxml')
+data = soup.find_all("div",class_ = "table-container yf-1jecxey")
+
+for tilte in data :
+    
+
+
