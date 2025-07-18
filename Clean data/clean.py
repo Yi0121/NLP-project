c91a7cde28486b7913bcbea9b578e72223d7df8e
boxbox <boxbox210077@gmail.com>
2025-07-18 16:09:49 +0800

    clean data

--- Clean data/clean.py
+++ Clean data/clean.py
@@ -1,7 +1,8 @@
 import pandas as pd
-df1 = pd.read_excel('台灣性別消費娛樂性產品(缺少24年資料).xlsx',sheet_name='收入與支出')
-df2 = pd.read_excel('台灣性別消費娛樂性產品(缺少24年資料).xlsx',sheet_name='9.休閒與文化')
-df1 = df1.rename(columns={
+import numpy as np
+
+df = pd.read_csv('台灣各地區消費娛樂性產品(缺少24年資料).csv')
+df = df.rename(columns={
     '1.食品及非酒精飲料' : '食品食品及非酒精飲料',
     '2.酒精飲料' : '酒精飲料',
     '3.菸草' : '菸草',
@@ -14,12 +15,43 @@ df1 = df1.rename(columns={
     '10.教育' : '教育',
     '11.餐廳及旅館' : '餐廳及旅館',
     '12.什項消費' : '什項消費',
+    '(1)套裝旅遊(不含自助旅遊)': '套裝旅遊',
+    '(2)娛樂消遣及文化服務': '娛樂消遣及文化服務',
+    '(3)書報雜誌文具': '書報雜誌文具',
+    '(4)教育消遣康樂器材及其附屬品': '教育消遣康樂器材及其附屬品'
+
 })
-df2 = df2.rename(columns={
-    '(1)套裝旅遊(不含自助旅遊)' : '套裝旅遊',
-    '(2)娛樂消遣及文化服務' : '娛樂消遣及文化服務',
-    '(3)書報雜誌文具' : '書報雜誌文具',
-    '(4)教育消遣康樂器材及其附屬品' : '教育消遣康樂器材及其附屬品'
-})
-df1.to_csv("台灣消費細項 by 性別.csv",index=False,encoding='utf-8-sig')
-df2.to_csv("娛樂細項 by 性別.csv",index=False,encoding='utf-8-sig')
+# dftest = pd.melt(
+#    df,
+#    id_vars=['年份','地區'],
+#    value_vars=['套裝旅遊', '娛樂消遣及文化服務', '書報雜誌文具','教育消遣康樂器材及其附屬品'],
+#    var_name = '休閒與文化類別',  # 新的變數欄位名稱
+#    value_name = '類別金額')
+# dftest.to_csv("娛樂分類各系項樞紐.csv", index = False)
+Region = df['地區'].unique().tolist()
+data = {
+    'RegionID' : list(range(1, len(Region) + 1)),
+    'RegionName' : Region
+}
+
+north_regions = ['台北市', '新北市', '基隆市', '桃園市', '新竹市', '新竹縣', '宜蘭縣']
+central_regions = ['台中市', '苗栗縣', '彰化縣', '南投縣', '雲林縣']
+south_regions = ['台南市', '高雄市', '屏東縣', '嘉義市', '嘉義縣']
+east_regions = ['花蓮縣', '台東縣']
+island_regions = ['澎湖縣']
+
+conditions = [
+    df['地區'].isin(north_regions),
+    df['地區'].isin(central_regions),
+    df['地區'].isin(south_regions),
+    df['地區'].isin(east_regions),
+    df['地區'].isin(island_regions)
+]
+
+# 3. 定義對應的選擇列表 (當條件滿足時要賦予的值)
+choices = ['北部', '中部', '南部', '東部', '離島']
+
+df['區域'] = np.select(conditions, choices, default='未分類')
+df.to_csv("區域消費.csv", index=False)
+
+
